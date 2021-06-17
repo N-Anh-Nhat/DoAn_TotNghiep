999e6edcf011670dd3680cc0aceb61197af87983
phung dep trai <phungle612000@gmail.com>
2021-06-17 10:38:22 +0700

    fix slideshow-user

--- WebUserShop/wwwroot/js/custom.js
+++ WebUserShop/wwwroot/js/custom.js
@@ -1,15 +1,6 @@
 (function($) {
 
 
-	$('#slides-shop').superslides({
-		inherit_width_from: '.cover-slides',
-		inherit_height_from: '.cover-slides',
-		play: 5000,
-		animation: 'fade',
-	});
-
-	$(".cover-slides ul li").append("<div class='overlay-background'></div>");
-
 
 	/* ..............................................
 	   Scroll
